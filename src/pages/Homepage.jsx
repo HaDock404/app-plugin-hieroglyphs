157e6606166f5821dbba0d9bfee616e20573bbce
HaDock404 <delescluse.g@gmail.com>
2024-11-13 08:27:06 +0100

    test components

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import Text from '../lib/Text'
+import Button from '../lib/Button'
+
+function Homepage() {
+    return (
+        <>
+            <Text />
+            <Button />
+        </>
+    )
+}
+
+export default Homepage
