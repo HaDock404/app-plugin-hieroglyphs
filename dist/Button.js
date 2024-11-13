bd835c5c329ffd2d80aa5d7a897b0cbf93061988
HaDock404 <delescluse.g@gmail.com>
2024-11-13 10:13:41 +0100

    organization name

---
+++ dist/Button.js
@@ -0,0 +1,10 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+function Button() {
+  return /*#__PURE__*/React.createElement("button", null, "Click me");
+}
+var _default = exports.default = Button;
---
+++ dist/index.js
@@ -0,0 +1,20 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Button", {
+  enumerable: true,
+  get: function () {
+    return _Button.default;
+  }
+});
+Object.defineProperty(exports, "Text", {
+  enumerable: true,
+  get: function () {
+    return _Text.default;
+  }
+});
+var _Text = _interopRequireDefault(require("./Text"));
+var _Button = _interopRequireDefault(require("./Button"));
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
