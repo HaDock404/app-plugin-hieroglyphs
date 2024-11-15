4953c7dbfa955e66af20288aa345fd02b238fe50
HaDock404 <delescluse.g@gmail.com>
2024-11-15 15:26:15 +0100

    new icons

--- src/lib/Text.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Text() {
-  return (
-    <div>hello</div>
-  )
-}
-
-export default Text;
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,39 @@
-import Text from "./Text";
-import Button from './Button'
-import ArrowTest from './ArrowTest'
-export { Text, Button, ArrowTest };
+import arrow_bend_down_left from './arrow-bend-down-left'
+import arrow_bend_down_right from './arrow-bend-down-right';
+import arrow_bend_left_down from './arrow-bend-left-down';
+import arrow_bend_left_up from './arrow-bend-left-up';
+import arrow_bend_right_down from './arrow-bend-right-down';
+import arrow_bend_right_up from './arrow-bend-right-up';
+import arrow_bend_up_left from './arrow-bend-up-left';
+import arrow_bend_up_right from './arrow-bend-up-right';
+import arrow_circle_down_left from './arrow-circle-down-left'
+import arrow_circle_down_right from './arrow-circle-down-right'
+import arrow_circle_down from './arrow-circle-down'
+import arrow_circle_left from './arrow-circle-left'
+import arrow_circle_right from './arrow-circle-right'
+import arrow_circle_up_left from './arrow-circle-up-left'
+import arrow_circle_up_right from './arrow-circle-up-right'
+import minus_circle from './minus-circle'
+import plus_circle from './plus-circle'
+import x_circle from './x-circle'
+
+export { 
+    arrow_bend_down_left,
+    arrow_bend_down_right,
+    arrow_bend_left_down,
+    arrow_bend_left_up,
+    arrow_bend_right_down,
+    arrow_bend_right_up,
+    arrow_bend_up_left,
+    arrow_bend_up_right,
+    arrow_circle_down_left,
+    arrow_circle_down_right,
+    arrow_circle_down,
+    arrow_circle_left,
+    arrow_circle_right,
+    arrow_circle_up_left,
+    arrow_circle_up_right,
+    minus_circle,
+    plus_circle,
+    x_circle
+ };
